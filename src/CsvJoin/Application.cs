bd54d6d86832d2c449b9d4f822abfa9b0ec55fdf
Mariusz <mariopl@poczta.onet.eu>
2021-04-17 11:52:39 +0200

    use linq for Path.Combine calls, replace DeserializeFromReader with DeserializeFromStream

--- src/CsvJoin/Application.cs
+++ src/CsvJoin/Application.cs
@@ -25,30 +25,26 @@ namespace CsvJoin
             string directory = args.First();
             string[] fileNames = args.Skip(1).Take(2).ToArray();
 
-            string path1 = Path.Combine(
-                Environment.CurrentDirectory,
-                directory,
-                fileNames[0]);
-            string path2 = Path.Combine(
-                Environment.CurrentDirectory,
-                directory,
-                fileNames[1]);
+            string[] paths = fileNames
+                .Select(fileName => Path.Combine(
+                    Environment.CurrentDirectory,
+                    directory,
+                    fileName))
+                .ToArray();
 
-            using var reader1 = File.OpenText(path1);
-            using var reader2 = File.OpenText(path2);
+            using var stream1 = File.OpenRead(paths[0]);
+            using var stream2 = File.OpenRead(paths[1]);
 
-            var csv1s = CsvSerializer.DeserializeFromReader<IEnumerable<Csv1>>(
-                reader1);
-            var csv2s = CsvSerializer.DeserializeFromReader<IEnumerable<Csv2>>(
-                reader2);
+            var csv1s = CsvSerializer.DeserializeFromStream<IEnumerable<Csv1>>(
+                stream1);
+            var csv2s = CsvSerializer.DeserializeFromStream<IEnumerable<Csv2>>(
+                stream2);
 
             var linq = _preparator.PrepareLeftJoinLinq(csv1s, csv2s);
 
             var output = Console.OpenStandardOutput();
 
-            using StreamWriter writer = new StreamWriter(output);
-
-            CsvSerializer.SerializeToWriter(linq, writer);
+            CsvSerializer.SerializeToStream(linq, output);
         }
     }
 }
