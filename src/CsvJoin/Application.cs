3b23ab379b172b2e001f58a684fe9369986ae0dc
Mariusz <mariopl@poczta.onet.eu>
2021-04-23 19:41:14 +0200

    group code so it's easier to read

--- src/CsvJoin/Application.cs
+++ src/CsvJoin/Application.cs
@@ -29,19 +29,16 @@ namespace CsvJoin
                 Environment.CurrentDirectory,
                 directory,
                 fileNames[0]);
-
             string path2 = Path.Combine(
                 Environment.CurrentDirectory,
                 directory,
                 fileNames[1]);
 
             using var stream1 = File.OpenRead(path1);
-
             using var stream2 = File.OpenRead(path2);
 
             var csv1s = CsvSerializer
                 .DeserializeFromStream<IEnumerable<Csv1>>(stream1);
-
             var csv2s = CsvSerializer
                 .DeserializeFromStream<IEnumerable<Csv2>>(stream2);
 
