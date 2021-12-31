01045a006b9e64a830c1d2ac1e06f30a291adf75
Mariusz Stępień <mariopl@poczta.onet.eu>
2021-12-31 22:11:59 +0100

    use object instead of dynamic, fix vs2022 build

--- src/CsvJoin/Services.Abstractions/ILinqPreparator.cs
+++ src/CsvJoin/Services.Abstractions/ILinqPreparator.cs
@@ -5,7 +5,7 @@ namespace CsvJoin.Services.Abstractions
 {
     public interface ILinqPreparator
     {
-        IEnumerable<dynamic> PrepareLeftJoinLinq(IEnumerable<Csv1> csv1s, IEnumerable<Csv2> csv2s);
-        IEnumerable<dynamic> PrepareJoinLinq(IEnumerable<Csv1> csv1s, IEnumerable<Csv2> csv2s);
+        IEnumerable<object> PrepareLeftJoinLinq(IEnumerable<Csv1> csv1s, IEnumerable<Csv2> csv2s);
+        IEnumerable<object> PrepareJoinLinq(IEnumerable<Csv1> csv1s, IEnumerable<Csv2> csv2s);
     }
 }
